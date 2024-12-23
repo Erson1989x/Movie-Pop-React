5d07e41288f18ce841bfee792ddf13edc0623355
Erhard Nagy <Erson1989x@gmail.com>
2024-12-23 15:50:56 +0200

    progress

---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '8px'    
+}
+
+const starContainerStyle = {
+    display: 'flex',
+    gap: '4px'
+}
+
+const textStyle = {
+    lineHeight: '1',
+    margin: '0'
+}
+const StarRating = ( { maxRating = 5} ) => {
+
+  return (
+    <div style={containerStyle}>
+        <div style={starContainerStyle}>
+            {
+                Array.from({length: maxRating}, (_, i) => 
+                    <span key={i}>⭐️</span>
+                )
+            }
+        </div>
+        <p style={textStyle}>10</p>
+    </div>
+  )
+}
+
+export default StarRating
