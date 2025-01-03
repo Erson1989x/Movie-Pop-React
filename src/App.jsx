1786cac7bc40e21a4ee61f21469e80abf0471feb
Erhard Nagy <Erson1989x@gmail.com>
2025-01-03 14:40:21 +0200

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,45 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Search from "./components/Navbar/Search";
 import NumResult from "./components/Navbar/NumResult";
-import { tempMovieData } from "../src/utils/tempMovieData";
-import { tempWatchedData } from "../src/utils/tempWatchedData";
 import Box from "./components/Hero/Box";
 import MovieList from "./components/Hero/MovieList";
 import Summary from "./components/Hero/Summary";
 import { average } from "./utils/fuctions";
 import WatchList from "./components/Hero/WatchList";
+import Loader from "./components/Loader/Loader";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const KEY = "c0d89336";
   const query = "man";
 
-  /* const fetchMovies = async () => {
-    try {
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-      const data = await res.json();
-      setMovies(data.Search);
-      console.log(data.Search);
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);*/
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+        const data = await res.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
+    fetchMovies();
+  }, []);
+
+  /*
   useEffect(() => {
     fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
     .then((res) => res.json())
     .then((data) => setMovies(data.Search));
   }, []);
-
+*/
 
   return (
     <>
@@ -57,7 +60,7 @@ export default function App() {
           }
         />*/}
         <Box>
-          <MovieList movies={movies} />
+          {isLoading ? <Loader /> : <MovieList movies={movies} />}
         </Box>
         <Box>
           <Summary watched={watched} average={average} />
